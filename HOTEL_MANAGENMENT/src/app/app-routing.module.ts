8c9b91314f49699076ddd9c8d0319b66c1315ca4
Samik1110 <samikshakhangar1999@gmail.com>
2023-04-28 20:57:32 +0530

    hgyiui

--- HOTEL_MANAGENMENT/src/app/app-routing.module.ts
+++ HOTEL_MANAGENMENT/src/app/app-routing.module.ts
@@ -1,29 +1,44 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { LandingComponent } from './landing/landing.component';
+// import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
+// import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+// import { AdminComponent } from './admin/admin.component';
+// import { OwnerComponent } from './owner/owner.component';
+// import { UserComponent } from './user/user.component';
+// import { CommonModule } from '@angular/common';
+// import { LoginSuccessComponent } from './admin/login-success/login-success.component';
+// import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
+
+// const routes: Routes = [
+//   { path : '', component:LandingComponent},
+//   { path: "landing", component : LandingComponent},
+//   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+//   { path: 'owner', loadChildren: () => import('./owner/owner.module').then(m => m.OwnerModule) },
+//   { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+//   {  path: 'sign-in-form' , component: SignInFormComponent},
+//   { path: 'sign-up-form' , component:SignUpFormComponent},
+//   { path : 'admin' , component: AdminComponent},
+//   { path : 'owner' , component: OwnerComponent},
+//   { path : 'user' , component: UserComponent},
+//   { path: 'loginsuccess', component: LoginSuccessComponent},
+//   {path : 'hoteldetails' , component: HotelDetailsComponent},
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes), CommonModule],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
-import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
-import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
-import { AdminComponent } from './admin/admin.component';
-import { OwnerComponent } from './owner/owner.component';
-import { UserComponent } from './user/user.component';
 
-const routes: Routes = [
-  // { path: "landing", component:LandingComponent},
-  { path: "landing" , loadChildren:()=> import('./landing/landing.module').then(mod=>mod.LandingModule)},
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'owner', loadChildren: () => import('./owner/owner.module').then(m => m.OwnerModule) },
-  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  {  path: 'sign-in-form' , component: SignInFormComponent},
-  { path: 'sign-up-form' , component:SignUpFormComponent},
-  { path : 'admin' , component: AdminComponent},
-  { path : 'owner' , component: OwnerComponent},
-  { path : 'user' , component: UserComponent}
-];
+const routes: Routes = [ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-
--- HOTEL_MANAGENMENT/src/app/app.module.ts
+++ HOTEL_MANAGENMENT/src/app/app.module.ts
@@ -1,29 +1,87 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { MatFormFieldModule ,MatLabel, MatError , MatFormFieldControl } from '@angular/material/form-field';
+// // import { MAT_FORM_FIELD } from '@angular/material/form-field';
+// import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
+// import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+// import {MatButtonModule} from '@angular/material/button';
+// import { MatCardModule} from '@angular/material/card';     
+// import { NgModel, ReactiveFormsModule } from '@angular/forms';
+// import { MatInputModule} from '@angular/material/input';
+// import { Sharetextpipe } from './sharetextpipe.pipe';
+// import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
+// import { HttpClientModule } from '@angular/common/http';
+// import { SharedModuleModule } from './shared-module/shared-module.module';
+// import { RouterModule } from '@angular/router';
+// import {MatRadioModule} from '@angular/material/radio';
+// import { MatIconModule } from '@angular/material/icon';
+// import { Router } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+// import { MatPseudoCheckboxModule } from '@angular/material/core';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     SignInFormComponent,
+//     SignUpFormComponent,
+//     Sharetextpipe,
+//     HotelDetailsComponent,
+    
+   
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     MatFormFieldModule,
+//     MatButtonModule,
+//     MatCardModule,
+//     ReactiveFormsModule,
+//     MatInputModule,
+//     HttpClientModule,
+//     RouterModule,
+//     MatRadioModule,
+//     MatIconModule,
+//     CommonModule,
+//     BrowserModule,
+//     MatPseudoCheckboxModule ,
+//     BrowserAnimationsModule ,
+//     MatCheckboxModule,
+//     NgModule
+//   ],
+
+//   exports : [
+//     MatInputModule, 
+//     Sharetextpipe,
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
-import { MAT_FORM_FIELD } from '@angular/material/form-field';
-import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
-import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
-import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
+  declarations:[
     AppComponent,
-    SignInFormComponent,
-    SignUpFormComponent,
-    BackButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    
-],
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
